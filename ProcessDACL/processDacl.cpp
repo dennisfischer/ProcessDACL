be3aa86d025d5f2ce98fe7f1df325da460e57c9e
Dennis Fischer <chaosking0912@gmail.com>
2016-01-12 22:26:13 +0100

    Updated ProcessDACL for thesis

--- ProcessDACL/processDacl.cpp
+++ ProcessDACL/processDacl.cpp
@@ -1,33 +1,42 @@
-// testProject.cpp : Defines the entry point for the console application.
-//
+#include "targetver.h"
 
-#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+#include <tchar.h>
 
+#include "Windows.h"
+#include "aclapi.h"
+#include "accctrl.h"
+#pragma comment(lib, "advapi32.lib")
+
+#include <SDKDDKVer.h>
 
 int main()
 {
-	
-	EXPLICIT_ACCESS eDeny;
-	DWORD dwAccessPermissions = GENERIC_WRITE | WRITE_DAC | DELETE | WRITE_OWNER | READ_CONTROL | PROCESS_CREATE_PROCESS | PROCESS_CREATE_THREAD
-		| PROCESS_QUERY_INFORMATION | PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_SET_INFORMATION | PROCESS_SET_QUOTA |
+	//The permissions that should be remaining. 
+	//Virtual memory access is no longer possible as PROCESS_VM_WRITE,
+	//PROCESS_VM_READ and PROCESS_VM_OPERATION are missing.
+	DWORD dwAccessPermissions = GENERIC_WRITE | WRITE_DAC | DELETE |
+		WRITE_OWNER | READ_CONTROL |
+		PROCESS_CREATE_PROCESS | PROCESS_CREATE_THREAD |
+		PROCESS_QUERY_INFORMATION | PROCESS_QUERY_LIMITED_INFORMATION |
+		PROCESS_SET_INFORMATION | PROCESS_SET_QUOTA |
 		PROCESS_SUSPEND_RESUME | PROCESS_TERMINATE | SYNCHRONIZE;
 
-	//get process handle
+	//Now build the DACL object with the Windows API functions
 	PACL pDacl = nullptr;
-
+	EXPLICIT_ACCESS eDeny;
 	BuildExplicitAccessWithName(&eDeny, TEXT("CURRENT_USER"), dwAccessPermissions, DENY_ACCESS, NO_INHERITANCE);
-	std::cout << SetEntriesInAcl(1, &eDeny, nullptr, &pDacl);
-	std::cout << std::endl << SetSecurityInfo(GetCurrentProcess(), SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, nullptr, nullptr, pDacl, nullptr);
-	std::cout << std::endl;
+	SetEntriesInAcl(1, &eDeny, nullptr, &pDacl);
+
+	//Now set the DACL to this process
+	SetSecurityInfo(GetCurrentProcess(), SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, nullptr, nullptr, pDacl, nullptr);
+
+	//Cleanup object
 	LocalFree(pDacl);
 
-
-	while(true)
-	{
-		
-		Sleep(1000);
-		std::cout << "Still running!" << std::endl;
-	}
-    return 0;
+	//Further code
+	//...
+	return 0;
 }
 
